7ecf67ab959ea03a3fcee7e774bed2c729555b03
HarshAhalpara-001 <ahalparaharsh1234@gmail.com>
2025-07-23 08:48:28 +0530

    scrapping testing file for scrapy

--- scraper_test.py
+++ scraper_test.py
@@ -2,14 +2,11 @@ import os
 import sys
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-
 # 👇 Set working directory to the Scrapy project
 scrapy_project_dir = os.path.join(os.getcwd(), "product_scraper", "product_scraper")
 os.chdir(scrapy_project_dir)
-
 # 👇 Add scrapy project to sys.path so imports work
 sys.path.append(scrapy_project_dir)
-
 # 👇 Now import the spider
 from spiders.universal_spider import UniversalSpider
 
